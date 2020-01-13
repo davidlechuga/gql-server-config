b0ae3280481cda556b304cfac41c3292aaa85ba4
davidlechuga <david.lechuga@lakmisystems.com.mx>
2020-01-13 13:04:29 -0600

    configuracion correcta de apolloserver con exprees

--- server/index.js
+++ server/index.js
@@ -1,24 +1,16 @@
 import express from 'express';
-// conectar graphql con express una vez 1) levantado servidor de app , y creado el schema.
-import graphqlHTTP from 'express-graphql';
-//importamos el schema
-import {schema} from './src/models/schema'
+// requerimos Apollo server para hacer la configuracion con sus dos parametros.
+// 1) parametro , importamos el schema
+import { typeDefs } from './src/models/schema'
+// 2) parametro importamos resolvers.
+import {resolvers} from './src/resolvers/resolvers'
+import { ApolloServer } from 'apollo-server-express';
 
 
 const app = express();
-// creamos una app con express en puerto 8000
-app.get("/", (request, response) => {
-    response.send('all ok')
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app })
+app.listen({ port: 4004 }, () => console.log(`El servidor esta corriendo http://localhost:4004${server.graphqlPath}`));
 
 
-// creamos una nueva ruta montada de app
-app.use('/graphql', graphqlHTTP({
-    //schema a utilizar cuando estemos en esta url
-    schema,
-    //inicializar graphql
-    graphiql: true 
-}));
-
-
-app.listen(8000, () => console.log('server listening'));
