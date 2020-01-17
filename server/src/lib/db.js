cd5c78600f509a6ed9f229034d7cae5fee0ac60d
davidlechuga <david.lechuga@lakmisystems.com.mx>
2020-01-16 18:48:09 -0600

    limits , and newresolvers added corrrectly

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -4,19 +4,16 @@ dotenv.config();
 
 mongoose.Promise = global.Promise;
 
-
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true , useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const clientesSchema = new mongoose.Schema({
-    nombre: String,
-    apellidos: String,
-    email: String,
-    job: String,
-    project: String
-})
+	nombre: String,
+	apellidos: String,
+	email: String,
+	job: String,
+	project: String
+});
 
-const Clientes = mongoose.model('lakmiClientes', clientesSchema)
+const Clientes = mongoose.model('lakmiClientes', clientesSchema);
 
-export { Clientes }
-
+export { Clientes };
--- server/src/resolvers/resolvers.js
+++ server/src/resolvers/resolvers.js
@@ -2,49 +2,61 @@ import mongoose from 'mongoose';
 import { Clientes } from '../lib/db';
 import { rejects } from 'assert';
 
-
 // son los mismos resolver que exportamos, pero YA CON sintaxis de "GRAPHQL-TOOLS"
 export const resolvers = {
-    Query: {
+	Query: {
+		getCliente: (root, { id }) => {
+			return new Promise((resolve, reject) => {
+				Clientes.findById(id, (error, cliente) => {
+					if (error) rejects(error);
+					else resolve(cliente);
+				});
+			});
+		},
 
-        getCliente: (root, { id }) => {
-            return new Promise((resolve, reject) => {
-                Clientes.findById(id, (error, cliente) => {
-                    if (error) rejects(error)
-                    else resolve(cliente)
-                });
-            });     
-        },
+		getClientes: (root, { limite }) => {
+			return Clientes.find({}).limit(limite);
+		}
+	},
 
-        getClientes: (root, { limite }) => {
-            return  Clientes.find({}).limit(limite)
-        },
-        
-    },
+	Mutation: {
+		// 1) generamos clientes con MUTATION que viene del Schema , y
+		// 2 ) id's dinamicos cada vez que se creen nuevos registros.
+		crearCliente: (root, { input }) => {
+			const nuevoCliente = new Clientes({
+				id: input.id,
+				nombre: input.nombre,
+				apellidos: input.apellidos,
+				email: input.email,
+				job: input.job,
+				project: input.project
+			});
+			// el id tambien se requiere, mongo lo agrega con un ._id
+			nuevoCliente.id = nuevoCliente._id;
+			// insertar registros a la base de datos
+			return new Promise((resolve, reject) => {
+				nuevoCliente.save((error) => {
+					if (error) reject(error);
+					else resolve(nuevoCliente);
+				});
+			});
+		},
 
-    Mutation: {
-        // 1) generamos clientes con MUTATION que viene del Schema , y
-        // 2 ) id's dinamicos cada vez que se creen nuevos registros.
-        crearCliente: (root, { input }) => {
-            const nuevoCliente = new Clientes({
-                id: id,
-                nombre: nombre,
-                apellidos: apellidos,
-                email: email,
-                job: job,
-                project: project
-                
-            });
-            // el id tambien se requiere, mongo lo agrega con un ._id
-            nuevoCliente.id = nuevoCliente._id;
-            // insertar registros a la base de datos
-            return new Promise((resolve, reject) => {
-                nuevoCliente.save((error) => {
-                    if (error) reject(error)
-                    else resolve(nuevoCliente)
-                })
-            })
-
-        },
-    }
+		actualizarCliente: (root, { input }) => {
+			return new Promise((resolve, reject) => {
+				Clientes.findOneAndUpdate({ _id: input.id }, input, { new: true }, (error, cliente) => {
+					if (error) reject(error);
+					else resolve(cliente);
+				});
+			});
+		},
+		eliminarCliente: (root, { id }) => {
+			return new Promise((resolve, reject) => {
+				Clientes.findOneAndRemove({ _id: id }, (error) => {
+					if (error) reject(error);
+					else resolve('se elimino correctamente');
+				});
+			});
+		}
+	}
 };
